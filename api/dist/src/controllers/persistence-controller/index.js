42a173f35294a1f76135e5b7548d1e7388a5a84b
Ievgen Melnychuk <ievgenmelnychuk@Ievgens-MacBook.local>
2019-10-22 02:24:37 -0400

    removed dist

--- api/dist/src/controllers/persistence-controller/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TransacitonPersistenceController_1 = require("./TransacitonPersistenceController");
-exports.chaseTransactionReader = TransacitonPersistenceController_1.chaseTransactionReader;
-//# sourceMappingURL=index.js.map
--- api/dist/src/models/errors/errors.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class TransactionRequestError {
-}
-exports.TransactionRequestError = TransactionRequestError;
-//# sourceMappingURL=errors.js.map
