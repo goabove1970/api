09f1b402a2975f2300d2336788d9e203e11d82a7
Ievgen Melnychuk <ievgenmelnychuk@ievgens-macbook.lan>
2020-05-15 17:11:35 -0400

    Removed unused classes

--- api/src/controllers/data-controller/chase/ChaseTransactionFileDataController.ts
+++ api/src/controllers/data-controller/chase/ChaseTransactionFileDataController.ts
@@ -1,16 +1,3 @@
-import { FileController } from '@controllers/data-controller/FileController';
-import { CachedDataController } from '@src/controllers/data-controller/CachedDataController';
 import { ChaseTransactionParser } from '@controllers/parser-controller/chase/ChaseTransactionParser';
-import { ChaseTransaction } from '@models/transaction/chase/ChaseTransaction';
 
 export const chaseTransactionParser: ChaseTransactionParser = new ChaseTransactionParser();
-
-export class ChaseTransactionFileDataController extends FileController<ChaseTransaction> {
-    constructor(filename: string) {
-        super(filename, chaseTransactionParser);
-    }
-}
-
-export const chaseTransactionFileDataController: CachedDataController<
-    ChaseTransaction
-> = new ChaseTransactionFileDataController('debit.csv');
