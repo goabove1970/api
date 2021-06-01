198ac9f204c773fc2b0374205b03ae1097e31232
goabove1970 <goabove1970@gmail.com>
2021-06-01 17:50:22 -0400

    increased pocket size

--- api/app.ts
+++ api/app.ts
@@ -19,12 +19,12 @@ import logger from '@src/logger';
 export const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
 
 app.use('/transactions', transactionsRouter);
 app.use('/users', userRouter);
