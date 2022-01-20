f7c9893ad175d50dd325c905d5d121420a348375
goabove1970 <goabove1970@gmail.com>
2022-01-20 17:02:29 -0500

    Updated packaging script

--- api/pack.js
+++ api/pack.js
@@ -1,19 +1,31 @@
 const { exec } = require('child_process');
+const { inspect } = require('util');
 const zipFolder = require('zip-folder');
 
 exec('node -p -e "require(\'./package.json\').version"', (error, version, stderr) => {
     exec('node -p -e "require(\'./package.json\').name"', (error, name, stderr) => {
-        const packageName = name.substr(0, name.length - 1);
+        const packageName = name.slice(0, name.length - 1);
         exec(`rm -rf ${packageName}-*.zip`, (error, stdout, stderr) => {
-            const versionStr = version.substr(0, version.length - 1);
+            const versionStr = version.slice(0, version.length - 1);
             console.log(`Packing ${packageName} version ${versionStr}`);
             const packageFileName = `${packageName}-${versionStr}.zip`;
-            zipFolder('.', packageFileName, function(err) {
-                if (err) {
-                    console.log('Packing failed', err);
-                } else {
-                    console.log('Packing complete');
-                }
+
+            exec('mkdir pack_tmp', () => {
+                console.log('Created pack_tmp for packaging');
+                exec('cp -r {dist,node_modules,package.json} ./pack_tmp/', () => {
+                    console.log('Copied source for packaging, packing...');
+                    zipFolder('./pack_tmp/', packageFileName, function(err) {
+                        if (err) {
+                            console.log(`Packing failed: ${inspect(err)}`);
+                        } else {
+                            console.log(`Packing complete`);
+                            console.log(`Removing pack_tmp`);
+                            exec('rm -r - f pack_tmp', () => {
+                                console.log(`Done`);
+                            });
+                        }
+                    });
+                });
             });
         });
     });
